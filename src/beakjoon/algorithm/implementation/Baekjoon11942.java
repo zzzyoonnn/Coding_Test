481d1527fdc702b4d38fdb1026bebee1dd269267
zyoonshin <dwsrh03@gmail.com>
2023-06-18 23:17:38 +0900

    solve bok - implementation

---
+++ src/beakjoon/algorithm/implementation/Baekjoon11942.java
@@ -0,0 +1,18 @@
+package beakjoon.algorithm.implementation;
+/*
+문제
+- 고려대학교는 사랑입니다.
+출력
+- 첫째 줄에 “고려대학교”를 출력하세요.
+ex 1)
+출력
+- 고려대학교
+ */
+import java.io.IOException;
+
+public class Baekjoon11942 {
+  public static void main(String[] agrs) throws IOException {
+
+    System.out.println("고려대학교");
+  }
+}
