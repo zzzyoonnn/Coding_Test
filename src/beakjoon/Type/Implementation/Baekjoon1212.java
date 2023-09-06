8d9732cdbe46b55c3a6677b152e879aaf813cf49
zyoonshin <dwsrh03@gmail.com>
2023-09-07 00:33:02 +0900

    Solved Implementation

---
+++ src/beakjoon/Type/Implementation/Baekjoon1212.java
@@ -0,0 +1,35 @@
+package beakjoon.Type.Implementation;
+/*
+문제
+- 8진수가 주어졌을 때, 2진수로 변환하는 프로그램을 작성하시오.
+입력
+- 첫째 줄에 8진수가 주어진다. 
+  주어지는 수의 길이는 333,334을 넘지 않는다.
+출력
+- 첫째 줄에 주어진 수를 2진수로 변환하여 출력한다. 
+  수가 0인 경우를 제외하고는 반드시 1로 시작해야 한다.
+ex 1)
+입력        출력
+- 314       - 11001100
+ */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baekjoon1212 {
+  public static void main(String[] agrs) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String s = br.readLine();
+    StringBuilder sb = new StringBuilder();
+    String[] binary = {"000", "001", "010", "011", "100", "101", "110", "111"};
+
+    for (int i = 0; i < s.length(); i++) {
+      int n = s.charAt(i) - '0';
+      sb.append(binary[n]);
+    }
+
+    if (sb.charAt(0) == '0') sb.deleteCharAt(0);
+    if (sb.charAt(0) == '0') sb.deleteCharAt(0);
+    System.out.println(sb);
+  }
+}
