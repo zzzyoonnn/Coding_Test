037564ecdac03f5acfdaf34933974199a71f1664
Jiyoon Shin <118426807+zzzyoonnn@users.noreply.github.com>
2024-01-15 22:55:48 +0900

    15th day

---
+++ src/programmers/level0/Page3_18.java
@@ -0,0 +1,19 @@
+package programmers.level0;
+// qr code
+import java.util.Scanner;
+
+public class Page3_18 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String code = sc.next();
+    int q = sc.nextInt();
+    int r = sc.nextInt();
+
+    String answer = "";
+
+    for (int i = 0; i < code.length(); i += m) {
+      if (i % q == r) answer += code.charAt(i);
+    }
+    System.out.println(answer);
+  }
+}
