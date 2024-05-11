019242b1348c6c592df8028d94eb90ff26b93292
zzzyoonnn <dwsrh03@gmail.com>
2024-05-12 00:39:18 +0900

    feat:implementation

--- src/beakjoon/algorithm/implementation/Baekjoon12873.java
+++ src/beakjoon/algorithm/implementation/Baekjoon12873.java
@@ -13,9 +13,11 @@ public class Baekjoon12873 {
     ArrayList<Integer> list = new ArrayList<>();
     for (int i = 1; i <= n; i++) list.add(i);
 
-    int currentIndex=0;
+    int currentIndex = 0;       // 리스트의 첫 번째 요소부터 제거하기 위해 인덱스를 0으로 초기화
     for(long i = 1; i < n; i++) {
-      currentIndex = (int)((currentIndex + i * i  * i - 1) % list.size());
+      currentIndex = (int)(((currentIndex + Math.pow(i, 3)) - 1) % list.size());
+      // currentIndex + Math.pow(i, 3)) - 1 : 리스트에서 제거할 요소의 위치
+      // % list.size() : 리스트를 순환하며 인덱스를 조정하기 위해
       list.remove(currentIndex);
     }
     System.out.println(list.get(0));
