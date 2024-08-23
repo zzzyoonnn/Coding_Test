7ac6db22fe8a1ec890e51e8bcb281f38bc61dd24
zzzyoonnn <dwsrh03@gmail.com>
2024-08-23 19:51:50 +0900

    feat:implementation

---
+++ src/beakjoon/algorithm/implementation/Baeckjoon10205.java
@@ -0,0 +1,27 @@
+package beakjoon.algorithm.implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baeckjoon10205 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int k = Integer.parseInt(br.readLine());
+    for (int i = 1; i <= k; i++) {
+      int h = Integer.parseInt(br.readLine());
+      String s = br.readLine();
+      for (int j = 0; j < s.length(); j++) {
+        char c = s.charAt(j);
+        if (c == 'b') h--;
+        else if (c == 'c') h++;
+
+        if (h == 0) break;
+      }
+
+      System.out.println("Data Set " + i + ":");
+      System.out.println(h);
+      System.out.println();
+    }
+  }
+}
