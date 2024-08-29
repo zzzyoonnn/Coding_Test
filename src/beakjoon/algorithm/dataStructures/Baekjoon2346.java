04ee3b1f05bd8e2205e8c41df00cb93658cd8436
zzzyoonnn <dwsrh03@gmail.com>
2024-08-29 23:59:41 +0900

    feat:implementation

--- src/beakjoon/algorithm/dataStructures/Baekjoon2346.java
+++ src/beakjoon/algorithm/dataStructures/Baekjoon2346.java
@@ -1,33 +1,45 @@
 package beakjoon.algorithm.dataStructures;
 
+import jdk.jshell.execution.JdiExecutionControlProvider;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Baekjoon2346 {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int n = Integer.parseInt(br.readLine());
-    List<Integer> list = new ArrayList<Integer>();
+    Deque<int[]> deque = new ArrayDeque<>();
 
     StringTokenizer st = new StringTokenizer(br.readLine());
-    for (int i = 0; i < n; i++) list.add(Integer.parseInt(st.nextToken()));
+    for (int i = 0; i < n; i++) deque.add(new int[] {i + 1, Integer.parseInt(st.nextToken())});
 
-    int index = 0;
-    while (!list.isEmpty()) {
-      int num = list.get(index);
-      System.out.println(list.remove(index));
+    StringBuilder sb = new StringBuilder();
+
+    while (!deque.isEmpty()) {
+      int num = deque.peek()[1];
+      sb.append(deque.pollFirst()[0]).append(" ");
+
+      System.out.println(num);
+      deque.removeFirst();
 
       if (num > 0) {
-        index = (index + num - 1) % list.size();
+        for (int i = 0; i < num; i++) {
+          deque.offerLast(deque.pollFirst());
+        }
       } else {
-        index = (index - (num % list.size()) + list.size()) % list.size();
-
+        for (int i = 0; i < Math.abs(num); i++) {
+          deque.addFirst(deque.pollLast());
+        }
       }
 
+      sb.append(deque.poll()[0]).append(" ");
+
     }
+
+    System.out.println(sb);
+
   }
 }
