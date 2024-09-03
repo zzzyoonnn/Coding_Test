5289fdf9165ad01c96578cba0f8380984aeeb890
zzzyoonnn <dwsrh03@gmail.com>
2024-09-04 00:03:12 +0900

    feat:implementation

--- src/beakjoon/algorithm/implementation/Baekjoon5464.java
+++ src/beakjoon/algorithm/implementation/Baekjoon5464.java
@@ -3,22 +3,53 @@ package beakjoon.algorithm.implementation;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Baekjoon5464 {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st = new StringTokenizer(br.readLine());
-    int n = Integer.parseInt(st.nextToken());   // 주차 공간들의 단위 무게당 요금
-    int m = Integer.parseInt(st.nextToken());   // 차량들의 무게
+    int n = Integer.parseInt(st.nextToken());   // number of parking spaces
+    int m = Integer.parseInt(st.nextToken());   // car's count
 
-    int[] parking = new int[n + 1];
+    // parking fee per spaces
+    int[] parkingFee = new int[n + 1];
+    int[] parkingStatus = new int[n + 1];
+    for (int i = 1; i <= n; i++) parkingFee[i] = Integer.parseInt(br.readLine());
 
-    ArrayList<Integer> list = new ArrayList<Integer>();
-    list.add(0);
-    for (int i = 1; i <= m; i++) list.add(Integer.parseInt(st.nextToken()));
+    // car's weight
+    int[] carWeight = new int[m + 1];
+    for (int i = 1; i <= m; i++) {
+      carWeight[i] = Integer.parseInt(br.readLine());
+    }
 
+    // Calculate parking fee
+    int total = 0;
+    Queue<Integer> queue = new LinkedList<>();
 
+    outer: for (int i = 0; i < m * 2; i++) {
+      int num = Integer.parseInt(br.readLine());
+      if (num > 0) {
+        for (int index = 1; index < parkingStatus.length; index++) {
+          if (parkingStatus[index] == 0) {
+            parkingStatus[index] = num;
+            continue outer;
+          }
+        }
+        queue.add(num);
+      } else {
+        num *= (-1);
+        for (int j = 1; j < parkingFee.length; j++) {
+          if (parkingStatus[j] == num) {
+            parkingStatus[j] = 0;
+            total += parkingFee[j] * carWeight[num];
+
+            if (!queue.isEmpty()) parkingStatus[j] = queue.poll();
+          }
+        }
+      }
+    }
+
+    System.out.println(total);
   }
 }
