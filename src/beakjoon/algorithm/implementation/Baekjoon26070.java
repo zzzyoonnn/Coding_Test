30c8f37ac3e73916b3b3e04ab9261e19859f831f
zzzyoonnn <dwsrh03@gmail.com>
2024-09-07 23:59:59 +0900

    feat:implementation

---
+++ src/beakjoon/algorithm/implementation/Baekjoon26070.java
@@ -0,0 +1,72 @@
+package beakjoon.algorithm.implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baekjoon26070 {
+  static long count;
+  static int a, b, c;
+  static int x, y, z;
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    a = Integer.parseInt(st.nextToken());   // chicken
+    b = Integer.parseInt(st.nextToken());   // pizza
+    c = Integer.parseInt(st.nextToken());   // hamburger
+
+    st = new StringTokenizer(br.readLine());
+    x = Integer.parseInt(st.nextToken());   // chicken's ticket
+    y = Integer.parseInt(st.nextToken());   // pizza's ticket
+    z = Integer.parseInt(st.nextToken());   // hamburger's ticket
+
+    count = 0;
+
+    calculateMax();
+
+    y += (x / 3);
+    x %= 3;
+
+    calculateMax();
+
+    z += (y / 3);
+    y %= 3;
+
+    calculateMax();
+
+    x += (z / 3);
+    z %= 3;
+
+    calculateMax();
+
+    System.out.println(count);
+  }
+
+  private static void calculateMax() {
+    for (int i = 0; i < 4; i++) {
+      int chicken = Math.min(a, x);
+      count += chicken;
+      a -= chicken;
+      x -= chicken;
+
+      //System.out.println(a + " " + b + " " + c + " " + x + " " + y + " " + z + " " + count);
+
+      int pizza = Math.min(b, y);
+      count += pizza;
+      b -= pizza;
+      y -= pizza;
+
+      //System.out.println(a + " " + b + " " + c + " " + x + " " + y + " " + z + " " + count);
+
+      int hamburger = Math.min(c, z);
+      count += hamburger;
+      c -= hamburger;
+      z -= hamburger;
+
+      //System.out.println(a + " " + b + " " + c + " " + x + " " + y + " " + z + " " + count);
+
+    }
+  }
+}
