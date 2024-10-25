e8b90a01dbb9b9254ca60058a5bfba491099071e
zzzyoonnn <dwsrh03@gmail.com>
2024-10-25 23:59:15 +0900

    feat: implementation

---
+++ src/beakjoon/algorithm/implementation/Baekjoon32184.java
@@ -0,0 +1,20 @@
+package beakjoon.algorithm.implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baekjoon32184 {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int a = Integer.parseInt(st.nextToken());
+    int b = Integer.parseInt(st.nextToken());
+
+    int count = (b - a) / 2 + 1;
+    if (a % 2 == 0 && b % 2 == 1) count += 2;
+
+    System.out.print(count);
+  }
+}
