8fc92d060c0d6512c8d83dc8026802eff95dcd71
zzzyoonnn <dwsrh03@gmail.com>
2024-12-23 23:40:23 +0900

    Dijkstra

--- src/beakjoon/algorithm/Dijkstra/Baekjoon17396.java
+++ src/beakjoon/algorithm/Dijkstra/Baekjoon17396.java
@@ -1,4 +1,4 @@
-package beakjoon.algorithm.ShortestPath;
+package beakjoon.algorithm.Dijkstra;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -11,18 +11,17 @@ import java.util.StringTokenizer;
 public class Baekjoon17396 {
   private static class Branch implements Comparable<Branch> {
     int connection;
-    int status;
-    int time;
+    long time;
 
-    public Branch(int connection, int status, int time) {
+    public Branch(int connection, long time) {
       this.connection = connection;
-      this.status = status;
       this.time = time;
     }
 
     @Override
     public int compareTo(Branch o) {
-      return this.time - o.time;
+      if (this.time - o.time > 0) return 1;    // Ascending sort
+      return -1;
     }
   }
 
@@ -33,15 +32,19 @@ public class Baekjoon17396 {
     int M = Integer.parseInt(st.nextToken());   // 분기점을 잇는 길의 수
 
     // 각 분기점이 적의 시야에 보이는가?(0: 안보임, 1: 보임)
-    char[] status = br.readLine().toCharArray();
+    int[] status = new int[N];
+    st = new StringTokenizer(br.readLine());
+    for (int index = 0; index < status.length; index++) {
+      status[index] = Integer.parseInt(st.nextToken());
+    }
 
     ArrayList<ArrayList<Branch>> branch = new ArrayList<>();
     for (int index = 0; index < N; index++) {
       branch.add(new ArrayList<>());
     }
 
-    int[] time = new int[N];
-    Arrays.fill(time, Integer.MAX_VALUE / 2);
+    long[] time = new long[N];
+    Arrays.fill(time, Long.MAX_VALUE);
 
     for (int info = 0; info < M; info++) {
       st = new StringTokenizer(br.readLine());
@@ -49,21 +52,21 @@ public class Baekjoon17396 {
       int b = Integer.parseInt(st.nextToken());
       int t = Integer.parseInt(st.nextToken());
 
-      branch.get(a).add(new Branch(b, status[b] - '0', t));
-      branch.get(b).add(new Branch(a, status[a] - '0', t));
+      branch.get(a).add(new Branch(b, t));
+      branch.get(b).add(new Branch(a, t));
     }
 
     PriorityQueue<Branch> pq = new PriorityQueue<>();
-    pq.offer(new Branch(0, 0, 0));
+    pq.offer(new Branch(0, 0));
     boolean[] visited = new boolean[N];
     time[0] = 0;
 
-    int answer = -1;
+    long answer = -1;
     while (!pq.isEmpty()) {
       Branch b = pq.poll();
       int now = b.connection;
-      System.out.println("now : " + now);
-      //System.out.println(b.time);
+      if (status[now] != 0 && now != N - 1) continue;
+      if (b.time > time[now]) continue;
       visited[now] = true;
 
       if (now == N - 1) {
@@ -72,10 +75,9 @@ public class Baekjoon17396 {
       }
 
       for (Branch child : branch.get(now)) {
-        System.out.println(child.connection);
-        if ((child.status == 0 || child.connection == N - 1) && !visited[child.connection] && time[child.connection] > child.time + time[now]) {
+        if ((status[child.connection] == 0 || child.connection == N - 1) && !visited[child.connection] && time[child.connection] > child.time + time[now]) {
           time[child.connection] = child.time + time[now];
-          pq.offer(new Branch(child.connection, child.status, time[child.connection]));
+          pq.offer(new Branch(child.connection, time[child.connection]));
         }
       }
     }
