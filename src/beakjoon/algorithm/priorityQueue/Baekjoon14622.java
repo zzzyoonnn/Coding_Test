9c3e38df3c2c57cf04aad58896ab6f450ff0fccd
zzzyoonnn <dwsrh03@gmail.com>
2025-01-16 23:56:50 +0900

    250116

--- src/beakjoon/algorithm/priorityQueue/Baekjoon14622.java
+++ src/beakjoon/algorithm/priorityQueue/Baekjoon14622.java
@@ -6,9 +6,10 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Baekjoon14622 {
-  static boolean turn;
-  static long dwScore, gsScore;
-  static HashSet<Integer> decimalSet;
+  static int turn;
+  static boolean[] check;
+  static long score;
+  static HashSet<Integer> decimalSet = new HashSet<>();
   static PriorityQueue<Integer> dwDecimal;
   static PriorityQueue<Integer> gsDecimal;
 
@@ -19,91 +20,91 @@ public class Baekjoon14622 {
     initialize();
 
     StringTokenizer st;
-    for (int fight = 0; fight < N; fight++) {
+    for (turn = 1; turn <= N; turn++) {
       st = new StringTokenizer(br.readLine());
       int d = Integer.parseInt(st.nextToken());
       int g = Integer.parseInt(st.nextToken());
 
-      System.out.println(decimalSet);
-      // 항상 대웅이부터 시작
-      // turn
-      turn = false;
+//      System.out.println(d + " " + g);
+//      System.out.print(check[d] + " " + check[g]);
+//      System.out.println(decimalSet);
       checkDecimal(d, dwDecimal);
-      turn = true;
       checkDecimal(g, gsDecimal);
-
-    System.out.println(dwScore + " " + gsScore);
 
     }
 
-
-    if (dwScore > gsScore) System.out.print("소수의 신 갓대웅");
-    else if (dwScore == gsScore) System.out.print("우열을 가릴 수 없음");
+    if (score > 0) System.out.print("소수의 신 갓대웅");
+    else if (score == 0) System.out.print("우열을 가릴 수 없음");
     else System.out.print("소수 마스터 갓규성");
-
   }
 
-  private static void isNotDecimal() {
-    if (!turn) {
+  // 소수가 아닐 경우
+  private static void isNotDecimal(PriorityQueue<Integer> pq) {
+    if (pq.equals(dwDecimal)) {  // 대웅이가 소수가 아닌 수를 말함
       if (gsDecimal.size() >= 3) {
         Queue<Integer> temp = new LinkedList<>();
         temp.add(gsDecimal.poll());
         temp.add(gsDecimal.poll());
-        gsScore += gsDecimal.poll();
+        score -= gsDecimal.poll();
 
         gsDecimal.offer(temp.poll());
         gsDecimal.offer(temp.poll());
       } else {
-        gsScore += 1000;
+        score -= 1000;
       }
-    } else {
-      {
-        if (dwDecimal.size() >= 3) {
-          Queue<Integer> temp = new LinkedList<>();
-          temp.add(dwDecimal.poll());
-          temp.add(dwDecimal.poll());
-          dwScore += dwDecimal.poll();
+    } else {  // 경수가 소수가 아닌 수를 말함
+      if (dwDecimal.size() >= 3) {
+        Queue<Integer> temp = new LinkedList<>();
+        temp.add(dwDecimal.poll());
+        temp.add(dwDecimal.poll());
+        score += dwDecimal.poll();
 
-          dwDecimal.offer(temp.poll());
-          dwDecimal.offer(temp.poll());
-        } else {
-          dwScore += 1000;
+        dwDecimal.offer(temp.poll());
+        dwDecimal.offer(temp.poll());
+      } else {
+        score += 1000;
+      }
+    }
+  }
+
+  // 소수일 경우
+  private static void isDecimal(int num, PriorityQueue<Integer> pq) {
+    if (!decimalSet.contains(num)) {
+//      System.out.println("소수 추가" + num);
+      decimalSet.add(num);
+//      System.out.println(decimalSet);
+      pq.offer(num);
+    } else {
+      if (pq.equals(dwDecimal)) score -= 1000;
+      else score += 1000;
+    }
+  }
+
+  // 소수인지 확인하기
+  private static void checkDecimal(int num, PriorityQueue<Integer> pq) {
+    if (check[num]) {
+      isDecimal(num, pq);
+    } else {
+      isNotDecimal(pq);
+    }
+  }
+
+
+  private static void initialize() {
+    score = 0;
+
+    check = new boolean[5000000];
+    Arrays.fill(check, true);
+    check[0] = false;
+    check[1] = false;
+    for (int index = 2; index * index < check.length; index++) {
+      if (check[index]) {
+        for (int j = index * index; j < check.length; j += index) {
+          check[j] = false;
         }
       }
     }
-  }
 
-  private static void isDecimal(int num, PriorityQueue<Integer> pq) {
-    if (decimalSet.contains(num)) {
-      if (!turn) dwScore -= 1000;
-      else gsScore -= 1000;
-    } else {
-      decimalSet.add(num);
-      pq.offer(num);
-    }
-  }
-
-  private static void checkDecimal(int num, PriorityQueue<Integer> pq) {
-    if (num <= 1) {   // 소수 아님
-      isNotDecimal();
-    }
-
-    if (num == 2) isDecimal(num, pq);
-
-    for (int i = 2; i < Math.sqrt(num); i++) {
-      if (num % i == 0) {
-        isNotDecimal();
-      }
-    }
-
-    isDecimal(num, pq);
-  }
-
-  private static void initialize() {
-    dwScore = 0;
-    gsScore = 0;
-
-    decimalSet = new HashSet<>();
     dwDecimal = new PriorityQueue<>(Comparator.reverseOrder());
     gsDecimal = new PriorityQueue<>(Comparator.reverseOrder());
   }
