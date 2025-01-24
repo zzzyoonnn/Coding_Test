c5cabd65f43c3a0d43bf36f24b8f69813834d7dd
zzzyoonnn <dwsrh03@gmail.com>
2025-01-24 23:59:29 +0900

    250124

--- src/beakjoon/algorithm/eratos/Baekjoon11502.java
+++ src/beakjoon/algorithm/eratos/Baekjoon11502.java
@@ -4,11 +4,12 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Baekjoon9020 {
+public class Baekjoon11502 {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int T = Integer.parseInt(br.readLine());
 
-    boolean[] check = new boolean[10001];
+    boolean[] check = new boolean[1001];
     check[0] = check[1] = true;
 
     for (int index = 2; index * index < check.length; index++) {
@@ -19,18 +20,25 @@ public class Baekjoon9020 {
       }
     }
 
-    int t = Integer.parseInt(br.readLine());
-    for (int tc = 0; tc < t; tc++) {
+    for (int tc = 0; tc < T; tc++) {
       int n = Integer.parseInt(br.readLine());
-      int a = 0, b = 0;
 
-      for (int i = 2; i <= n / 2; i++) {
-        if (!check[i] && !check[n - i]) {
-          a = i;
-          b = n - i;
+      for (int i = 2; i <= n; i++) {
+        if (check[i]) continue;
+
+        for (int j = 2; j <= n; j++) {
+          if (check[j]) continue;
+
+          for (int k = 2; k <= n; k++) {
+            if (check[k]) continue;
+
+            if (i + j + k == n) {
+              System.out.println(i + " " + j + " " + k);
+              break;
+            }
+          }
         }
       }
-      System.out.println(Math.min(a, b) + " " + Math.max(a, b));
     }
   }
 }
