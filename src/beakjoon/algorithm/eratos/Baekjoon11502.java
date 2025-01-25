5d26ed21098149cef41c77ca2bba7a3852ef09f6
zzzyoonnn <dwsrh03@gmail.com>
2025-01-25 23:40:23 +0900

    250125

--- src/beakjoon/algorithm/eratos/Baekjoon11502.java
+++ src/beakjoon/algorithm/eratos/Baekjoon11502.java
@@ -3,42 +3,54 @@ package beakjoon.algorithm.eratos;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class Baekjoon11502 {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int T = Integer.parseInt(br.readLine());
 
-    boolean[] check = new boolean[1001];
-    check[0] = check[1] = true;
+    boolean[] isNotPrime = new boolean[1001];
+    isNotPrime[0] = isNotPrime[1] = true;
 
-    for (int index = 2; index * index < check.length; index++) {
-      if (!check[index]) {
-        for (int i = index * index; i < check.length; i += index) {
-          check[i] = true;
+    for (int index = 2; index * index < isNotPrime.length; index++) {
+      if (!isNotPrime[index]) {
+        for (int i = index * index; i < isNotPrime.length; i += index) {
+          isNotPrime[i] = true;
         }
       }
     }
 
+    ArrayList<Integer> primes = new ArrayList<>();
+    for (int i = 2; i <= 1000; i++) {
+      if (!isNotPrime[i]) {
+        primes.add(i);
+      }
+    }
+
+    StringBuilder sb = new StringBuilder();
     for (int tc = 0; tc < T; tc++) {
       int n = Integer.parseInt(br.readLine());
+      boolean check = false;
 
       for (int i = 2; i <= n; i++) {
-        if (check[i]) continue;
-
-        for (int j = 2; j <= n; j++) {
-          if (check[j]) continue;
-
-          for (int k = 2; k <= n; k++) {
-            if (check[k]) continue;
-
-            if (i + j + k == n) {
-              System.out.println(i + " " + j + " " + k);
-              break;
+        if (check) break;
+        for (int j = i; j <= n; j++) {
+          for (int k = j; k <= n; k++) {
+            if (!isNotPrime[i] && !isNotPrime[j] && !isNotPrime[k]) {
+              if (i + j + k == n) {
+                sb.append(i).append(" ").append(j).append(" ").append(k).append("\n");
+                check = true;
+              }
             }
           }
+          if (check) break;
         }
+        if (check) break;
       }
+      if (!check) sb.append(0).append("\n");
     }
+
+    System.out.print(sb.toString());
   }
 }
