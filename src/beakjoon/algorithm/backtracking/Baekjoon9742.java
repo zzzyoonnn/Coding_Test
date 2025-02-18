f1b4102ffb200a63ebfa570aae04b29b1af831af
zzzyoonnn <dwsrh03@gmail.com>
2025-02-18 15:21:11 +0900

    250218

--- src/beakjoon/algorithm/backtracking/Baekjoon9742.java
+++ src/beakjoon/algorithm/backtracking/Baekjoon9742.java
@@ -6,30 +6,36 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Baekjoon9742 {
-  static int count;
+  static int target, count;
   static char[] chars;
   static boolean[] visited;
-  static StringBuilder st;
 
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st;
 
     String s;
+    StringBuilder sb = new StringBuilder();
     while ((s = br.readLine()) != null && !s.isEmpty()) {
       st = new StringTokenizer(s);
       String str = st.nextToken();
       chars = str.toCharArray();
-      count = Integer.parseInt(st.nextToken());
+      visited = new boolean[chars.length];
+      target = Integer.parseInt(st.nextToken());
 
-      System.out.println(backTracking(new StringBuilder(), 0));
+      count = 0;
+      sb.append(str).append(" ").append(target).append(" = ").append(backTracking(new StringBuilder(), 0)).append('\n');
     }
+    System.out.print(sb);
   }
 
   private static String backTracking(StringBuilder current, int now) {
-    if (count == now) {
-      String result = new String(chars);
-      return result;
+    if ((current.length() == chars.length)) {
+      count++;
+
+      if (target == count) {
+        return current.toString();
+      }
     }
 
     for (int i = 0; i < chars.length; i++) {
@@ -37,13 +43,15 @@ public class Baekjoon9742 {
 
       visited[i] = true;
       current.append(chars[i]);
+      String result = backTracking(current, now + 1);
 
-      backTracking(current, now + 1);
+      if (!result.equals("No permutation")) {
+        return result;
+      }
 
       current.deleteCharAt(current.length() - 1);
       visited[i] = false;
     }
-
     return "No permutation";
   }
 }
