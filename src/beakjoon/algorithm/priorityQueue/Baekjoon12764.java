fae236e5b0cb4f9eb9d7fee8d3664fba4300a6d1
zzzyoonnn <dwsrh03@gmail.com>
2025-03-04 23:59:22 +0900

    250304

--- src/beakjoon/algorithm/priorityQueue/Baekjoon12764.java
+++ src/beakjoon/algorithm/priorityQueue/Baekjoon12764.java
@@ -3,9 +3,7 @@ package beakjoon.algorithm.priorityQueue;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Baekjoon12764 {
   private static class Usage implements Comparable<Usage> {
@@ -26,46 +24,65 @@ public class Baekjoon12764 {
     }
   }
 
+  private static class User implements Comparable<User> {
+    int index;
+    int usage;
+    int endTime;
+
+    public User(int index, int usage, int endTime) {
+      this.index = index;
+      this.usage = usage;
+      this.endTime = endTime;
+    }
+
+    @Override
+    public int compareTo(User o) {
+      return endTime - o.endTime;
+    }
+  }
+
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     int N = Integer.parseInt(br.readLine());
 
     StringTokenizer st;
     PriorityQueue<Usage> usagePq = new PriorityQueue<>();
-    PriorityQueue<Integer> computerPq = new PriorityQueue<>();
+    PriorityQueue<User> computerPq = new PriorityQueue<>();
+
     for (int i = 0; i < N; i++) {
       st = new StringTokenizer(br.readLine());
-
       int P = Integer.parseInt(st.nextToken());
       int Q = Integer.parseInt(st.nextToken());
-
       usagePq.add(new Usage(P, Q));
     }
 
-    int maxComputer = -1;
-    int size = 0;
-    StringBuilder sb = new StringBuilder();
-    HashMap<Integer, Integer> map = new HashMap<>();
+    int maxComputer = 0;
+    int index = 0;
+
     while (!usagePq.isEmpty()) {
       Usage now = usagePq.poll();
 
-      if (computerPq.isEmpty() || now.startTime < computerPq.peek()) {
-        sb.append(size).append(' ');
-        size = 0;
-        computerPq.add(now.endTime);
+      if (computerPq.isEmpty() || now.startTime < computerPq.peek().endTime) {
+        computerPq.add(new User(index, 1, now.endTime));
         maxComputer = Math.max(maxComputer, computerPq.size());
-
-        map.put(now.endTime, 1);
+        index++;
       } else {
-        computerPq.poll();
-        computerPq.add(now.endTime);
-        size++;
+        User user = computerPq.poll();
+        computerPq.add(new User(user.index, ++user.usage, now.endTime));
       }
     }
 
     System.out.println(maxComputer);
-    for (int i : map.keySet()) {
-      System.out.print(i + " ");
+
+    TreeMap<Integer, Integer> treeMap = new TreeMap<>();
+    while (!computerPq.isEmpty()) {
+      User user = computerPq.poll();
+      System.out.println(user.index + " " + user.usage);
+      treeMap.put(user.index, user.usage);
+    }
+
+    for (int usage : treeMap.values()) {
+      System.out.print(usage + " ");
     }
   }
 }
